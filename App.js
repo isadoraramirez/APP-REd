610e8d7d9f944427dce66b0ac2153e299c87dcde
elyramirez <eramirez@redmagisterial.com>
2021-03-19 20:33:35 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  
+
+  return (
+    <SafeAreaView >
+      <NavigationContainer>
+       <Text>HOLOoooooasdo</Text> 
+      </NavigationContainer>
+      
+    </SafeAreaView>
+  );
+};
+
+
+
+export default App;
